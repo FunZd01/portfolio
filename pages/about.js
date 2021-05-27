02b3ed115dd274003b184ea4ee07cd434f3b08d4
funzd <domingofunez99@gmail.com>
2021-05-26 18:56:58 -0600

    presentacion definida

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,10 @@
 function About(){
-    return <div>About</div>
+    return (
+        <div>
+            <h1>ABOUT</h1>
+            <h3>hola bienvenidos mi nombre es <span>Domingo Funez</span></h3>
+        </div>
+    )
 }
 
 export default About
