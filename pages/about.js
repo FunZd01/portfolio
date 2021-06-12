e79df94630eed1a49b4adb194c1959b45ace214b
funzd <domingofunez99@gmail.com>
2021-06-12 11:52:32 -0600

    agregando experiencia

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,7 @@ import styles from '../styles/Home.module.css';
 import {Container, Row, Col} from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
 import {exp} from '../skills';
+import {data} from '../experience';
 
 const About = () => (
     <Layout>
@@ -10,21 +11,23 @@ const About = () => (
             <Container className={"mb-2"}>
                 <Row className={"bg-info p-2 justify-content-center rounded"}>
                     <Col className={"mb-2"} xs={10} md={3}>
-                        <Image src={"profile.jpg"} alt={"foto de perfil"} roundedCircle={true} thumbnail={true}
-                               width={"200"} >
+                        <Image className={"border border-dark"} src={"profile.jpg"} alt={"foto de perfil"}
+                               roundedCircle={true} thumbnail={true}
+                               width={"200"}>
                         </Image>
                     </Col>
                     <Col className={"text-center"} xs={12} md={5}>
                         <h3>BACKEND DEVELOPER</h3>
                         autodidacta, responsable y amante de los desafios que requieran usar el ingenio!<br/>
-                        <button type={"button"} className={"btn btn-dark rounded-pill p-2 mt-5"} style={{width: '100%' }}>
+                        <button type={"button"} className={"btn btn-dark rounded-pill p-2 mt-5"}
+                                style={{width: '100%'}}>
                             CONTRATAR!
                         </button>
                     </Col>
                 </Row>
                 <Row>
                     <Col xs={12} md={3} className={"rounded p-2 border border-info bg-light"}>
-                        <h1>Habilidades</h1>
+                        <h1 className={"text-center"}>Habilidades</h1>
                         {
                             exp.map(({skill, percentage}, i) => (
                                 <div className="py-3" key={i}>
@@ -38,15 +41,32 @@ const About = () => (
                             ))
                         }
                     </Col>
-                    <Col xs={12} md={6} className={"rounded p-2 bg-info"}>
-                        <h1>Experiencia</h1>
-
+                    <Col xs={12} md={6} className={"rounded p-2 bg-dark  text-light"}>
+                        <h1 className={"text-center"}>Experiencia</h1>
+                        {
+                            data.map(({cargo, company, duration, year, yearf, desc}, i) => (
+                                <div className={"py-3 border border-info rounded m-2"} key={i}>
+                                    <div className={"blockquote"}>
+                                        <h5>{cargo}
+                                            <span className={"text-muted"}> at {company}</span>
+                                        </h5>
+                                        <footer className={"blockquote-footer"}>
+                                            {duration}, ({year} - {yearf})
+                                        </footer>
+                                    </div>
+                                    <h6>
+                                        {desc}
+                                    </h6>
+                                </div>
+                            ))
+                        }
                     </Col>
                     <Col xs={12} md={3} className={"rounded p-2 border border-info bg-light"}>
-                        <h1>intereses</h1>
+                        <h1 className={"text-center"}>intereses</h1>
                         <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet debitis dolore earum
                             error excepturi impedit magnam odit suscipit tenetur totam vitae, voluptate voluptatibus?
-                            Esse iste quas rem repellat voluptas.</div>
+                            Esse iste quas rem repellat voluptas.
+                        </div>
                         <div>Alias, aspernatur aut dicta doloremque nesciunt odio omnis quas sed similique ut. A ad
                             consequuntur, culpa cumque doloremque enim eos facere ipsam itaque nisi nostrum officia quam
                             temporibus unde velit?
