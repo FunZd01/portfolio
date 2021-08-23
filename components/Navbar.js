96947673104c2901b31b8d66282fd35f90b334af
funzd <domingofunez99@gmail.com>
2021-08-22 18:42:34 -0600

    Agregando ruta activa

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,25 +1,24 @@
-import Link from "next/link";
 import { Navbar, Container } from 'react-bootstrap';
 import { useState } from 'react';
+import Link from '../components/Enlace.js'
 
 const Navbars = () => (
-        <Navbar bg="info" className={"rounded border-bottom border-light"} style={{ height: "10vh" }} expand="lg" fixed={"top"}>
-            <Container>
+        <Navbar bg="secondary" className={"rounded border-bottom border-dark"} style={{ height: "10vh" }} expand="lg" fixed={"top"}>
+           <Container>
                 <Link href="/">
-                    <Navbar.Brand><a>Domingo Funez</a></Navbar.Brand>
+                    <Navbar.Brand>Domingo Funez</Navbar.Brand>
                 </Link>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" className={"border border-dark rounded"} />
                 <Navbar.Collapse id="basic-navbar-nav" className={"justify-content-end"}>
-                    <ul className="navbar-nav bg-info p-2">
+                    <ul className="navbar-nav p-2 bg-secondary hoverable" style={{opacity: '0.6'}}>
                         <li className="nav-item">
-                            <Link href={"/"}>
-                                <a className="nav-link p-2 b-3 active"
-                                    style={{ boxShadow: "3px 4px 5px" }}>Inicio</a>
+                            <Link activeClassName='active' href='/'>
+                              <a className='nav-link p-2 b-3'>Home</a>
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link href={"/about"}>
-                                <a className="nav-link p-2 b-3 active" style={{ boxShadow: "3px 4px 5px" }}>CV</a>
+                            <Link activeClassName='active' href='/about'>
+                              <a className='nav-link p-2 b-3'>datos</a>
                             </Link>
                         </li>
                     </ul>
